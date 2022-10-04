d8af0ae8b448e46f2243153c6c10521214fdf2ec
gautam <gautam200.rejoice@gmail.com>
2022-10-04 12:48:42 +0530

    Add redux perist in cartDemo project

--- cartdemo/src/redux/store.js
+++ cartdemo/src/redux/store.js
@@ -4,12 +4,24 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import productSaga from "./productSaga";
 // import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from 'redux-saga';
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key : "persist-store",
+    storage
+}
+const persistReducers = persistReducer(persistConfig , rootReducer)
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-    rootReducer,
+    persistReducers,
     composeWithDevTools(applyMiddleware(sagaMiddleware))
 )
 sagaMiddleware.run(productSaga);
+
 export default store;
+
+export const persistor = persistStore(store)
